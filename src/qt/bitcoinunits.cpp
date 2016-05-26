ddb7be67b9616e02d75a9cb319ec7ba48a39621a
wbchen99 <wangbor.chen@gmail.com>
2016-05-26 11:33:58 +0800

    陳望博
    BTC HNT

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -40,9 +40,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("BTC");
-    case mBTC: return QString("mBTC");
-    case uBTC: return QString::fromUtf8("μBTC");
+    case BTC: return QString("HNT"); //BTC
+    case mBTC: return QString("mHNT");
+    case uBTC: return QString::fromUtf8("μHNT");
     default: return QString("???");
     }
 }
@@ -51,9 +51,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("Bitcoins");
-    case mBTC: return QString("Milli-Bitcoins (1 / 1" THIN_SP_UTF8 "000)");
-    case uBTC: return QString("Micro-Bitcoins (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
+    case BTC: return QString("Hnotes"); //Bitcoins
+    case mBTC: return QString("Milli-Hnotes (1 / 1" THIN_SP_UTF8 "000)");
+    case uBTC: return QString("Micro-Hnotes (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
     default: return QString("???");
     }
 }
