71df613e5b0a4fe1eb284a3a5ba228b5771b0c3f
wbchen99 <wangbor.chen@gmail.com>
2016-05-26 10:59:40 +0800

    Update spendfrom.py
    8332 2882
    18332 2882

--- contrib/spendfrom/spendfrom.py
+++ contrib/spendfrom/spendfrom.py
@@ -35,10 +35,10 @@ def check_json_precision():
 def determine_db_dir():
     """Return the default location of the bitcoin data directory"""
     if platform.system() == "Darwin":
-        return os.path.expanduser("~/Library/Application Support/Bitcoin/")
+        return os.path.expanduser("~/Library/Application Support/Hnote/")
     elif platform.system() == "Windows":
-        return os.path.join(os.environ['APPDATA'], "Bitcoin")
-    return os.path.expanduser("~/.bitcoin")
+        return os.path.join(os.environ['APPDATA'], "Hnote")
+    return os.path.expanduser("~/.hnote")
 
 def read_bitcoin_config(dbdir):
     """Read the bitcoin.conf file from dbdir, returns dictionary of settings"""
@@ -67,7 +67,7 @@ def connect_JSON(config):
     testnet = config.get('testnet', '0')
     testnet = (int(testnet) > 0)  # 0/1 in config file, convert to True/False
     if not 'rpcport' in config:
-        config['rpcport'] = 12881 if testnet else 2881
+        config['rpcport'] = 12882 if testnet else 2882
     connect = "http://%s:%s@127.0.0.1:%s"%(config['rpcuser'], config['rpcpassword'], config['rpcport'])
     try:
         result = ServiceProxy(connect)
